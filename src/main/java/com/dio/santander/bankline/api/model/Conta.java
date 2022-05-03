a2af2d1ed3fb079da6274f5be5baec7c9e462137
Rafael Silva <rafa.daitx.silva@rede.ulbra.br>
2022-05-03 13:36:02 -0300

    Mapeamento de entidades com JPA

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,14 +1,22 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	@Column(name = "conta_numero")
 	private Long numero;
+	
+	@Column(name = "conta_saldo")
+	private Double saldo;
+	
 	public Long getNumero() {
 		return numero;
 	}
 	public void setNumero(Long numero) {
 		this.numero = numero;
 	}
-	private Double saldo;
 	
 	public Double getSaldo() {
 		return saldo;
